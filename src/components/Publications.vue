50ff5472c058cdd4a0f7cc9436ebf527592f3e73
youseegreen <satoyushi0114@gmail.com>
2020-06-27 12:06:49 +0900

    publication update

--- src/components/Publications.vue
+++ src/components/Publications.vue
@@ -5,16 +5,27 @@
         <h1 class="display-1 font-weight-bold mb-5">Publications</h1>
       </v-flex>
 
+      <!-- 論文誌 -->    
+      <v-flex xs12 mb-5>
+      <h1 class="display-0 mb-2">論文誌</h1>
+      <v-divider class="mb-3"></v-divider>
+      <ol>
+        <li class="headline mb-2">
+          <b>Yushi Sato</b>, Takefumi Hiraki, Naruki Tanabe, Haruka Matsukura, Daisuke Iwai, and Kosuke Sato, "Modifying Texture Perception with Pseudo-Haptic Feedback for a Projected Virtual Hand Interface," <i>IEEE Access</i>, 16pages. (to appear)
+        </li> 
+      </ol>
+      </v-flex>
+
       <!-- 国際会議 -->    
       <v-flex xs12 mb-5>
       <h1 class="display-0 mb-2">国際会議</h1>
       <v-divider class="mb-3"></v-divider>
       <ol>
         <li class="headline mb-2">
-          <b>Yushi Sato</b>, Naruki Tanabe, Kohei Morita, Takefumi Hiraki, Parinya Punpongsanon, Haruka Matsukura, Daisuke Iwai, Kosuke Sato, "Pseudo-Haptic Feedback in a Projected Virtual Hand for Tactile Perception of Textures," In <i>Adjunct Proceedings of the 2019 IEEE World Haptics Conference (WHC 2019)</i>, pp.WP1P.09:1-2, July, 2019.
+          <b>Yushi Sato</b>, Naruki Tanabe, Kohei Morita, Takefumi Hiraki, Parinya Punpongsanon, Haruka Matsukura, Daisuke Iwai, and Kosuke Sato, "Pseudo-Haptic Feedback in a Projected Virtual Hand for Tactile Perception of Textures," In <i>Adjunct Proceedings of the 2019 IEEE World Haptics Conference (WHC 2019)</i>, pp.WP1P.09:1-2, July, 2019.
         </li> 
         <li class="headline mb-2">
-          <b>Yushi Sato</b>, Naruki Tanabe, Kohei Morita, Takefumi Hiraki, Parinya Punpongsanon, Haruka Matsukura, Daisuke Iwai, Kosuke Sato, "Pseudo-Haptic Feedback in a Projected Virtual Hand for Tactile Perception of Textures," <i>The 2019 IEEE World Haptics Conference (WHC 2019)</i>, Hands-on Demos, DM2.09, July, 2019.
+          <b>Yushi Sato</b>, Naruki Tanabe, Kohei Morita, Takefumi Hiraki, Parinya Punpongsanon, Haruka Matsukura, Daisuke Iwai, and Kosuke Sato, "Pseudo-Haptic Feedback in a Projected Virtual Hand for Tactile Perception of Textures," <i>The 2019 IEEE World Haptics Conference (WHC 2019)</i>, Hands-on Demos, DM2.09, July, 2019.
         </li> 
         <li class="headline mb-2">
           Naruki Tanabe, <b>Yushi Sato</b>, Kohei Morita, Michiya Inagaki, Yuichi Fujino, Parinya Punpongsanon, Haruka Matsukura, Daisuke Iwai, and Kosuke Sato, "fARFEEL: Providing Haptic Sensation of Touched Objects using Visuo-Haptic Feedback," In <i>Proceedings of the 26th IEEE Conference on Virtual Reality and 3D User Interfaces (IEEE VR 2019), pp.1355-1356, March, 2019.
@@ -34,7 +45,7 @@
           <b>佐藤優志</b>，平木剛史，田辺育暉，松倉悠，岩井大輔，佐藤宏介，"投影手インタフェースの疑似触覚フィードバックによる材質感呈示手法"，質感のつどい第5回公開フォーラム（ポスター発表），2019年12月．
         </li> 
         <li class="headline mb-2">
-          <b>佐藤優志</b>，田辺育暉，森田耕平，プンポンサノン パリンヤ，松倉悠，岩井大輔，佐藤宏介，"投影バーチャルハンドの疑似触覚フィードバックによる接触感呈示"，第63回システム制御情報学会研究発表講演会講演論文集(SCI'19)，p.1371-1378，2019年5月．
+          <b>佐藤優志</b>，田辺育暉，森田耕平，プンポンサノン パリンヤ，松倉悠，岩井大輔，佐藤宏介，"投影バーチャルハンドにおける疑似触覚フィードバックによる接触感呈示"，第63回システム制御情報学会研究発表講演会講演論文集(SCI'19)，p.1371-1378，2019年5月．
         </li> 
         <li class="headline mb-2">
           森田耕平，田辺育暉，<b>佐藤優志</b>，プンポンサノン パリンヤ，松倉悠，岩井大輔，佐藤宏介，"頭部方向を用いた投影型拡張仮想手の操作範囲の拡張"，第63回システム制御情報学会研究発表講演会講演論文集(SCI'19)，p.1375-1381，2019年5月．
@@ -54,10 +65,10 @@
       <v-divider class="mb-3"></v-divider>
       <ol>
         <li class="headline mb-2">
-          2020年度学会賞「奨励賞」 (<b>佐藤優志</b>，田辺育暉，森田耕平，プンポンサノン パリンヤ，松倉悠，岩井大輔，佐藤宏介，"投影バーチャルハンドの疑似触覚フィードバックによる接触感呈示"，システム制御情報学会)，2020年3月受賞.
+          2020年度学会賞「奨励賞」 (<b>佐藤優志</b>，田辺育暉，森田耕平，プンポンサノン パリンヤ，松倉悠，岩井大輔，佐藤宏介，"投影バーチャルハンドにおける疑似触覚フィードバックによる接触感呈示"，システム制御情報学会)，2020年3月受賞.
         </li> 
         <li class="headline mb-2">
-          SCI学生発表賞 (<b>佐藤優志</b>，田辺育暉，森田耕平，プンポンサノン パリンヤ，松倉悠，岩井大輔，佐藤宏介，"投影バーチャルハンドの疑似触覚フィードバックによる接触感呈示"，システム制御情報学会)，2010年5月受賞.
+          SCI学生発表賞 (<b>佐藤優志</b>，田辺育暉，森田耕平，プンポンサノン パリンヤ，松倉悠，岩井大輔，佐藤宏介，"投影バーチャルハンドにおける疑似触覚フィードバックによる接触感呈示"，システム制御情報学会)，2010年5月受賞.
         </li> 
         <li class="headline mb-2">
           ファーウェイ賞 (<b>佐藤優志</b>，"バーチャルハンドのPseudo-Haptic Feedbackによる材質感呈示手法"，第8回サイエンス・インカレ)，2019年3月受賞.
