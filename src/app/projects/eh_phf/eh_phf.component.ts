bddc1112b83c0e2a5addc87ca4340906252006bf
youseethree <y.sato@sens.sys.es.osaka-u.ac.jp>
2021-08-11 16:05:48 +0900

    add manholes and modify each project page

--- src/app/projects/eh_phf/eh_phf.component.ts
+++ src/app/projects/eh_phf/eh_phf.component.ts
@@ -12,4 +12,22 @@ export class EH_PHFComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  //https://blog.beatdjam.com/entry/2018/06/22/190000
+  copy_bibtex(paper_name){
+    const tmp = document.createElement('textarea');
+    if(paper_name == 'access'){
+      tmp.textContent = '@article{sato2020modifying,\n  title={Modifying texture perception with pseudo-haptic feedback for a projected virtual hand interface},\n  author={Sato, Yushi and Hiraki, Takefumi and Tanabe, Naruki and Matsukura, Haruka and Iwai, Daisuke and Sato, Kosuke},\n  journal={IEEE Access},\n  volume={8},\n  pages={120473--120488},\n  year={2020},\n  publisher={IEEE}\n}';
+    }
+    else{
+    tmp.textContent = '@inproceedings{sato2019pseudo,\n  title={Pseudo-haptic feedback in a projected virtual hand for tactile perception of textures},\n  author={Sato, Yushi and Tanabe, Naruki and Morita, Kohei and Hiraki, Takefumi and Punpongsanon, Parinya and Matsukura, Haruka and Iwai, Daisuke and Sato, Kosuke},\n  booktitle={Proc. IEEE World Haptics Conf.},\n  pages={1--2},\n  year={2019}\n}';
+    }
+    document.body.appendChild(tmp);
+    document.getSelection().selectAllChildren(tmp);
+    document.execCommand("Copy");
+    document.body.removeChild(tmp);
+    alert("クリップボードにコピーしました！\n\nコピーしたテキスト：\n" + tmp.textContent);
+
+  }
+
 }
--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -26,45 +26,45 @@ const routes: Routes = [
             {
                 path: 'eh_phf', component: EH_PHFComponent,
                 data: {
-                    title: 'シェッフェの一対比較法の浦の変法の元論文について | 佐藤 優志 (Yushi Sato)',
-                    description: 'シェッフェの一対比較法の浦の変法の元論文について。',
-                    keyword: '佐藤優志,youseegreen,Yushi Sato,シェッフェの一対比較法,浦の変法,参考文献',
+                    title: 'Pseudo-haptic feedback for ExtendedHand | 佐藤 優志 (Yushi Sato)',
+                    description: '投影バーチャルハンドインタフェースに対する疑似触覚フィードバックの応用',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,ExtendedHand,projected virtual hand,pseudo-hatpic feedback,tactile sensation',
                     ogUrl: 'https://youseegreen.github.io/projects/eh_phf'
                 }
             },
             {
                 path: 'farfeel', component: fARFEELComponent,
                 data: {
-                    title: 'ぷよべ～す（ぷよ譜生成ソフト）の公開 | 佐藤 優志 (Yushi Sato)',
-                    description: 'ぷよぷよの対戦動画からぷよ譜を自動生成するソフトを公開しました。',
-                    keyword: '佐藤優志,youseegreen,Yushi Sato,ぷよぷよ,ぷよ譜,ぷよぷよクロニクル,c++,OpenCV',
+                    title: 'fARFEEL | 佐藤 優志 (Yushi Sato)',
+                    description: 'fARFEEL: Providing Haptic Sensation of Touched Objects Using Visuo-Haptic Feedback',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,fARFEEL,projected virtual hand,teleexistence,telepresence,haptic feedback,pseudo-haptic feedback',
                     ogUrl: 'https://youseegreen.github.io/projects/farfeel'
                 }
             },
             {
                 path: 'responsive_eh', component: Responsive_EHComponent,
                 data: {
-                    title: 'ぷよべ～す（ぷよ譜生成ソフト）の公開 | 佐藤 優志 (Yushi Sato)',
-                    description: 'ぷよぷよの対戦動画からぷよ譜を自動生成するソフトを公開しました。',
-                    keyword: '佐藤優志,youseegreen,Yushi Sato,ぷよぷよ,ぷよ譜,ぷよぷよクロニクル,c++,OpenCV',
+                    title: 'Responsive-ExtendedHand | 佐藤 優志 (Yushi Sato)',
+                    description: 'Responsive-ExtendedHand : 投影バーチャルハンドの実物体接触に対して適切な視覚効果を付与するシステム',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,Responsive-ExtendedHand,projected virtual hand,pseudo-hatpic feedback,deep learning',
                     ogUrl: 'https://youseegreen.github.io/projects/responsive_eh'
                 }
             },
             {
                 path: 'desktophand', component: DesktopHandComponent,
                 data: {
-                    title: 'ぷよべ～す（ぷよ譜生成ソフト）の公開 | 佐藤 優志 (Yushi Sato)',
-                    description: 'ぷよぷよの対戦動画からぷよ譜を自動生成するソフトを公開しました。',
-                    keyword: '佐藤優志,youseegreen,Yushi Sato,ぷよぷよ,ぷよ譜,ぷよぷよクロニクル,c++,OpenCV',
+                    title: 'DesktopHand | 佐藤 優志 (Yushi Sato)',
+                    description: 'DesktopHand : デスクトップ画面上に表示したバーチャルハンドにより円滑なコミュニケーションを支援するシステム',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,DesktopHand,virtual Hand',
                     ogUrl: 'https://youseegreen.github.io/projects/desktophand'
                 }
             },
             {
                 path: 'hagbot', component: HagbotComponent,
                 data: {
-                    title: 'ぷよべ～す（ぷよ譜生成ソフト）の公開 | 佐藤 優志 (Yushi Sato)',
-                    description: 'ぷよぷよの対戦動画からぷよ譜を自動生成するソフトを公開しました。',
-                    keyword: '佐藤優志,youseegreen,Yushi Sato,ぷよぷよ,ぷよ譜,ぷよぷよクロニクル,c++,OpenCV',
+                    title: 'Hagbot | 佐藤 優志 (Yushi Sato)',
+                    description: 'Hagbot : ハグインタラクションにおけるロボットの動き方によるコミュニケーション手法の調査',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,Hagbot,hag interaction,emotion',
                     ogUrl: 'https://youseegreen.github.io/projects/hagbot'
                 }
             },
